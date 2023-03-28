1244d3e85df23ee860df44b95bdf841ade2be22f
duc <duc@azsystem.fr>
2023-03-28 18:51:04 +0200

    2nd KATA: inventaire de biere

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
+## Doc
+
+https://www.golangprograms.com/go-language/variables.html
+https://go.dev/doc/tutorial/getting-started
+https://go.dev/ref/spec
+
+## TODO
+
+comment representer une caisse ?
+comment afficher notre caisse ?
+comment gérer une rêgle d'achat du type "2 achetés un offert"
+comment acheter un produit ? ( le soustraire du stock et ajouter sa valeur à la caisse )
+
 ## KATA 1
-
-
-par exemple : les bières en tchéquie
 
 comment faire commentaire en go?   //exemple /* multiline */
 comment faire un hello world en go ? Done
 comment representer un stock de produit ? Done
 comment afficher notre liste de produit ? Done
-comment acheter un produit ? ( le soustraire du stock et ajouter sa valeur à la caisse )
-
-
-https://go.dev/doc/tutorial/getting-started
-https://go.dev/ref/spec
 
 ## KATA 2
-Fix git!!!
-La bite?
-comment representer une caisse ?
-comment afficher notre caisse ?
-comment gérer une rêgle d'achat du type "2 achetés un offert"
+Fix git!!! Good
+La bite? Narf explain ~~ ( la bite sert a plein de choses, mais surtout a ne pas mettre de "var" )
+Representation de l'inventaire des bieres (nom, prix, quantiter)
+
--- hello.go
+++ hello.go
@@ -5,24 +5,33 @@ import (
 	"fmt"
 )
 
-func main() {
-	var beers = []string{"Svijany", "Kozel", "Bernard", "Radegast"}
-	var beerStock = []int{5, 20, 12, 1}
+type BigBeerStock struct {
+	name     string
+	price    float64
+	quantity uint
+}
 
-	fmt.Println("Hello", ",World!", "Hello", "again")
-	fmt.Println(beers[0])
-	for i := 0; i < 4; i++ { //TODO La bite ??
-		fmt.Println("Il reste", beerStock[i], "de la biere", beers[i], "!")
+func main() {
+	//KATA 1
+	//var beers = []string{"Svijany", "Kozel", "Bernard", "Radegast"}
+	//var beerStock = []int{5, 20, 12, 1}
+	//fmt.Println("Hello", ",World!", "Hello", "again")
+	//fmt.Println(beers[0])
+	//for i := 0; i < 4; i++ { //TODO La bite ??
+	//	fmt.Println("Il reste", beerStock[i], "de la biere", beers[i], "!")
+	//}
+
+	//KATA 2
+	fmt.Println(BigBeerStock{"Svijany", 2.50, 20})
+	var svijany = BigBeerStock{"Svijany", 2.50, 20}
+	var beers = []BigBeerStock{
+		svijany,
+		{"Kozel", 3.50, 12},
+		{"Bernard", 1.80, 42},
+		{"Radegast", 2.70, 27},
 	}
 
-	//SystemPropertiesAdvanced.exe
-	//"editor.parameterHints.enabled": false
-
-	/*
-		beerStock := list.New()
-		beerStock.PushFront("Svijany")
-		beerStock.PushFront("Kozel")
-		beerStock.PushFront("Bernard")
-		beerStock.PushFront("Radegast")
-	*/
+	for i := 0; i < len(beers); i++ {
+		fmt.Println("Il reste", beers[i].quantity, "de la biere", beers[i].name, "en stock !")
+	}
 }
